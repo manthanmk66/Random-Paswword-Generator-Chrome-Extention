eed141dbe9ddaec86e7173d875cddbe26b176004
Manthan Kasle <manthankaslemk@gmail.com>
2024-03-10 12:35:30 +0530

    chore:Added

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import logo from './logo.svg';
+import './App.css';
+import './style.css';
+
+function App() {
+  return (
+    <div className="App">
+    
+
+     {/* Password text and Copy */}
+     <div className="header">
+
+     
+     <div >njw7362%6</div>
+     <button className='btn-cpy' onclick>Copy</button>
+
+     </div>
+
+
+
+     {/* Character Length */}
+
+
+
+
+     {/*Strength*/}
+
+
+
+
+
+     {/*Generate Button*/}
+
+
+
+
+
+    </div>
+  );
+}
+
+export default App;
