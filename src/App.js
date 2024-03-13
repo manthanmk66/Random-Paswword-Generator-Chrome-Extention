ed5856ced433e0c37bef69bee8ea2ce92197cfd4
Manthan Kasle <manthankaslemk@gmail.com>
2024-03-13 16:16:55 +0530

    Added button and StrengthCheck

--- src/App.js
+++ src/App.js
@@ -1,92 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
-import './style.css';
+import "./styles.css";
+import { useState } from "react";
+import usePasswordGenerator from "./hooks/usepassword.js";
+import PasswordStrengthIndicator from "./components/StrengthChecker";
+import Button from "./components/Button.js";
+import Checkbox from "./components/Checkbox";
 
-function App() {
+export default function App() {
+  const [length, setLength] = useState(4);
+  const [checkboxData, setCheckboxData] = useState([
+    { title: "Include Uppercase Letters", state: false },
+    { title: "Include Lowercase Letters", state: false },
+    { title: "Include Numbers", state: false },
+    { title: "Include Symbols", state: false }
+  ]);
+  const [copied, setCopied] = useState(false);
 
+  const handleCheckboxChange = (i) => {
+    const updatedCheckboxData = [...checkboxData];
+    updatedCheckboxData[i].state = !updatedCheckboxData[i].state;
+    setCheckboxData(updatedCheckboxData);
+  };
 
-  const checkboxData=[
-    {title:"Include Uppercase Letters",state:false},
-    {title:"Include Lowercase Letters",state:false},
-    {title:"Include Numbers",state:false},
-    {title:"Include Symbols",state:false},
+  const handleCopy = () => {
+    navigator.clipboard.writeText(password);
+    setCopied(true);
 
-  ]
+    setTimeout(() => {
+      setCopied(false);
+    }, 1000);
+  };
+
+  const { password, errorMessage, generatePassword } = usePasswordGenerator();
+
   return (
-    <div className="App">
-    
-
-     <div className="container">
-     <div className="header">
-      {/* Password text and Copy */}
-     
-     <div >njw7362%6</div>
-     <button className='btn-cpy' onclick>Copy</button>
-
-     </div>
-
-     {/* Character Length */}
-     <div className="charlength">
-      <span>
-        <label> Character Length</label>
-        <label> 4</label>
-      </span>
-
-    
-        <input 
-        type='range'
-        min='4'
-        max='20'
-     
-        // onchange={}
-    
+    <div className="container">
+      {/* Password Text and Copy */}
+      {password && (
+        <div className="header">
+          <div className="title">{password}</div>
+          <Button
+            text={copied ? "Copied" : "copy"}
+            onClick={handleCopy}
+            customClass="copyBtn"
+          />
+        </div>
+      )}
+      {/* Character Length */}
+      <div className="charlength">
+        <span>
+          <label>Character Length</label>
+          <label>{length}</label>
+        </span>
+        <input
+          type="range"
+          min="4"
+          max="20"
+          value={length}
+          onChange={(e) => setLength(e.target.value)}
         />
-
       </div>
-
-      {/* CheckBoxex */}
-      <div className="checkboxex">
-        {checkboxData.map((checkbox,index)=>{
-          return <div key={index}>
-
-            <input
-            type="checkbox" checked={checkbox.state}/>
-            <label>{checkbox.title}</label>
-          </div>
+      {/* Checkboxes */}
+      <div className="checkboxes">
+        {checkboxData.map((checkbox, index) => {
+          return (
+            <Checkbox
+              key={index}
+              title={checkbox.title}
+              onChange={() => handleCheckboxChange(index)}
+              state={checkbox.state}
+            />
+          );
         })}
-
-        
-
       </div>
+      {/* Strength */}
+      <PasswordStrengthIndicator password={password} />
 
+      {/* Error Handling */}
+      {errorMessage && <div className="errorMessage">{errorMessage}</div>}
 
-
-
-     {/*Strength*/}
-
-
-
-
-
-     {/*Generate Button*/}
-
-     <button className='generateBtn'>Generate Password</button>
-
-
-
-
-
-
-
-
-
-     </div>
-
-
-
-
+      {/* Generate Button */}
+      <Button
+        text="Generate Password"
+        onClick={() => generatePassword(checkboxData, length)}
+        customClass="generateBtn"
+      />
     </div>
   );
 }
-
-export default App;
